a6562b098047a64c11aaee219dda7eefdbf5ae0f
吴忠辉 <40905218+darcy-shimmer@users.noreply.github.com>
2019-10-29 22:03:03 +0800

    Update srs_core.hpp

--- trunk/src/core/srs_core.hpp
+++ trunk/src/core/srs_core.hpp
@@ -20,7 +20,13 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
+//added by darcy 
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+extern std::map<int, string> infoCli;
+extern std::int placeHolder;
 #ifndef SRS_CORE_HPP
 #define SRS_CORE_HPP
 
