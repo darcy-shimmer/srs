ef0a0fa1c4e7674923d2e3d00d7e60ef94ef7866
吴忠辉 <40905218+darcy-shimmer@users.noreply.github.com>
2019-11-01 09:23:39 +0800

    Update srs_core.cpp

--- trunk/src/core/srs_core.cpp
+++ trunk/src/core/srs_core.cpp
@@ -22,6 +22,5 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include <srs_core.hpp>
-std::map<int, string> infoCli;
-std::int placeHolder = 0;
+map<int, string> infoCli;
 
